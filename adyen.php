2c3e9921660daa7614df9d909d7f08366c5b548b
Patrick Figel <pfigel@greenpeace.org>
2024-07-18 15:36:16 +0200

    Upgrade civix

--- adyen.php
+++ adyen.php
@@ -29,59 +29,12 @@ function adyen_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function adyen_civicrm_postInstall() {
-  _adyen_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function adyen_civicrm_uninstall() {
-  _adyen_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function adyen_civicrm_enable() {
   _adyen_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function adyen_civicrm_disable() {
-  _adyen_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function adyen_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _adyen_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function adyen_civicrm_entityTypes(&$entityTypes) {
-  _adyen_civix_civicrm_entityTypes($entityTypes);
 }
 
 /**
